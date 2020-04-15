6e62743b3282f0a0ef08f9a9ba4484b95585586e
Lenashalaumova <63418840+Lenashalaumova@users.noreply.github.com>
2020-04-15 22:02:11 +0300

    Добавила новую задачу
    Сортировка по сумме цифр

---
+++ falling.py
@@ -0,0 +1,15 @@
+from random import randint
+
+def sum (n):
+    sum=0
+    for i in range (0,len(a)):
+        sum+=n + i
+    return sum
+
+a=[randint (0,100) for i in range (10)]
+print(a)
+
+b = sorted ( a, reverse = True )
+
+print(b)
+
