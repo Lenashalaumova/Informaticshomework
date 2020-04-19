6372fda1b49995bbec953c4ea8b0b06c4459b3e3
Lenashalaumova <63418840+Lenashalaumova@users.noreply.github.com>
2020-04-19 19:27:36 +0300

    Сортировка выбором
    Не сохранилось

--- Bubble metod.py
+++ Bubble metod.py
@@ -1,3 +1,4 @@
+#метод пузырька например: 9 2 6 5 4  8
 from random import randint
 A=[randint (0,100) for i in range (10)]
 print (A)
@@ -5,7 +6,8 @@ print (A)
 N=10
 
 for i in range(N):
-    for j in range(N-i-1):
-        if A[j+1] < A[j]:
-            A[j], A[j+1] = A[j+1], A[j]
+    for j in range(N-i-1): #шаг
+        if A[j+1] < A[j]: #если меньше: 2 9 6 5 4 8
+            A[j], A[j+1] = A[j+1], A[j] #поменять местами
 print (A)
+#2 8 6 5 4 9, 2 4 6 5 8 9, 2 4 5 6 8 9
